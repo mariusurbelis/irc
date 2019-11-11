24518ba8e5d6ac6a5c493184e96bda77ee9e6121
Marius Urbelis <mariusurbelis23@gmail.com>
2019-11-11 21:52:24 +0000

    Currently supports multi-user connections with different usernames

--- client.py
+++ client.py
@@ -3,21 +3,24 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host ="127.0.0.1"
-port = 65432
-s.connect((host,port))
+host = "127.0.0.1"
+port = 3456
+s.connect((host, port))
 
-username = input('Please enter a username: ')
+# Print server's welcome message
+print("\n-------------------")
+print(s.recv(1024).decode())
+print("-------------------\n")
+
+username = input('Choose your username: ')
+s.sendall(bytes(username, 'utf-8'))
 
 def ts(r):
-   message1 = username + ":" + r
-   message2 = bytes(message1, 'utf-8')
-   s.sendall(message2)
-   data = s.recv(1024).decode()
-   print (data)
+    message2 = bytes(r, 'utf-8')
+    s.sendall(message2)
 
-while 2:
-   r = input('Enter: ')
-   ts(r)
+while True:
+    r = input('> ')
+    ts(r)
 
-s.close ()
+s.close()
