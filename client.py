02683d4df7567d22220cdae86a37f35b22d57fc3
Marius Urbelis <mariusurbelis23@gmail.com>
2019-12-03 12:12:47 +0000

    Client/bot changes

--- client.py
+++ client.py
@@ -1,19 +1,59 @@
-#! /usr/bin/python3
-
 import socket
+import sys
+import time
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = "irc.urbelis.dev"
+class IRC:
+ 
+    irc = socket.socket()
+  
+    def __init__(self):
+        # Define the socket
+        self.irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ 
+    def send(self, channel, msg):
+        # Transfer data
+        self.irc.send(bytes("PRIVMSG " + channel + " " + msg + "\n", "UTF-8"))
+ 
+    def connect(self, server, port, channel, botnick, botpass, botnickpass):
+        # Connect to the server
+        print("Connecting to: " + server)
+        self.irc.connect((server, port))
+
+        # Perform user authentication
+        self.irc.send(bytes("USER " + botnick + " " + botnick +" " + botnick + " :python\n", "UTF-8"))
+        self.irc.send(bytes("NICK " + botnick + "\n", "UTF-8"))
+        self.irc.send(bytes("NICKSERV IDENTIFY " + botnickpass + " " + botpass + "\n", "UTF-8"))
+        time.sleep(5)
+
+        # join the channel
+        self.irc.send(bytes("JOIN " + channel + "\n", "UTF-8"))
+ 
+    def get_response(self):
+        time.sleep(1)
+        # Get the response
+        resp = self.irc.recv(2040).decode("UTF-8")
+ 
+        if resp.find('PING') != -1:                      
+            self.irc.send(bytes('PONG ' + resp.split().decode("UTF-8") [1] + '\r\n', "UTF-8")) 
+ 
+        return resp
+
+
+## IRC Config
+server = "irc.urbelis.dev" # Provide a valid server IP/Hostname
 port = 3456
-s.connect((host, port))
-
-s.sendall("CAP LS 302\n".encode())
-s.sendall("NICK ProBot\n".encode())
-s.sendall("USER ProBot\n".encode())
-
-s.sendall("JOIN #test\n".encode())
+channel = "#test"
+botnick = "PROBot"
+botnickpass = "botbot"
+botpass = "<%= @guido_password %>"
+irc = IRC()
+irc.connect(server, port, channel, botnick, botpass, botnickpass)
 
 while True:
-    i = 1
+    text = irc.get_response()
+    print(text)
 
-s.close()
+    irc.send(channel, "Fuck!")
+ 
+    if "PRIVMSG" in text and channel in text and "hello" in text:
+        irc.send(channel, "Fuck!")
